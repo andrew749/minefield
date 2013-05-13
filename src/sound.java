be01cc6c40a62e0b779f1e81523a93c9ff191b41
andrew749 <andrewcod749@gmail.com>
2013-05-13 14:14:22 -0400

    Create sound.java
    created a sound class that can be used to play all the sounds available in the game.

---
+++ src/sound.java
@@ -0,0 +1,48 @@
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class sound {
+  static Clip clip;
+
+	/*
+	 * have an int variable that determines the type of sound that will be
+	 * played
+	 */
+	public sound(int type){
+		if (type == 1) {
+			try {
+				AudioInputStream audioInputStream = AudioSystem
+						.getAudioInputStream(new File("data/challa.wav")
+								.getAbsoluteFile());
+				clip = AudioSystem.getClip();
+				clip.open(audioInputStream);
+			} catch (Exception e) {
+			}
+		} else if (type == 2) {
+			try {
+				AudioInputStream audioInputStream = AudioSystem
+						.getAudioInputStream(new File("data/explosionsound.wav")
+								.getAbsoluteFile());
+				clip = AudioSystem.getClip();
+				clip.open(audioInputStream);
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	public static void playSound() {
+		try {
+			clip.start();
+		} catch (Exception ex) {
+			System.out.println("Error with playing sound.");
+			ex.printStackTrace();
+		}
+	}
+
+	public static void pause() {
+		clip.stop();
+	}
+}
